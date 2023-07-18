7f4fe8c0e15f5bfe34dcf403a6d943d2d0ec3c6f
coolcatpixel <atriv@newsasnow.com>
2023-07-18 17:31:57 -0400

    styled up things that look good

--- scripts.js
+++ scripts.js
@@ -5,11 +5,11 @@ if (!localStorage.getItem('counter')) {
 function count() {
     let counter = localStorage.getItem('counter')
     counter++;
-    document.querySelector('h2').innerHTML = counter;
+    document.querySelector('#h1').innerHTML = counter;
     localStorage.setItem('counter', counter);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
    document.querySelector('button').onclick = count;
-   document.querySelector('h2').innerHTML = localStorage.getItem('counter');
+   document.querySelector('#h1').innerHTML = localStorage.getItem('counter');
 });
