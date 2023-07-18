735986db349bc4c7cd42144576e57a96adcf795c
coolcatpixel <atriv@newsasnow.com>
2023-07-18 16:02:02 -0400

    added counter function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
+function count() {
+    let counter = localStorage.getItem('counter')
+    counter++;
+    document.querySelector('h2').innerHTML = counter;
+    localStorage.setItem('counter', counter);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+   document.querySelector('button').onclick = count;
+   document.querySelector('h2').innerHTML = localStorage.getItem('counter');
+});
